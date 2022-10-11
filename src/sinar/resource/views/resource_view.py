ef34203f49d4a875589db11a42fa65dcd4ef1e15
Khairil Yusof <khairil.yusof@gmail.com>
2022-10-11 15:07:02 +0800

    Add lookup title function for resource_type id for views re #2

--- src/sinar/resource/views/resource_view.py
+++ src/sinar/resource/views/resource_view.py
@@ -2,7 +2,8 @@
 
 from plone.dexterity.browser.view import DefaultView
 from sinar.resource import _
-
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -11,6 +12,19 @@ class ResourceView(DefaultView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('resource_view.pt')
+
+    def resource_type_title(self):
+
+        factory = getUtility(IVocabularyFactory,
+                             'sinar.resource.ResourceType')
+
+        vocabulary = factory(self)
+        if self.context.resource_type:
+            term = vocabulary.getTerm(self.context.resource_type)
+            return term.title
+        else:
+            return None
+
 
     def __call__(self):
         # Implement your own actions:
