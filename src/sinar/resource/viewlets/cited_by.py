59195f31d6860a9f1e87e52687baa7c127ef28bb
Khairil Yusof <khairil.yusof@gmail.com>
2023-09-05 23:16:57 +0800

    Add Cited By viewlet table
    Re: https://github.com/Sinar/sinarngo/issues/12

--- src/sinar/resource/viewlets/cited_by.py
+++ src/sinar/resource/viewlets/cited_by.py
@@ -1,15 +1,45 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from collective.relationhelpers import api as api_relations
+from plone import api
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 
 
 class CitedBy(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
+    def resource_type_title(self,value):
 
-    def get_message(self):
-        return u'My message'
+        factory = getUtility(IVocabularyFactory,
+                             'sinar.resource.ResourceType')
+
+        vocabulary = factory(self)
+        term = vocabulary.getTerm(value)
+
+        return term.title
+
+    def cited_by(self):
+        "Get objects that link to this item"
+        return api_relations.backrelations(self.context)
+
+    def cited_by_links(self):
+        "Get Links for objects that link to this item"
+
+        citations = api_relations.backrelations(self.context)
+
+        links = []
+
+        for cited in citations:
+            context = cited
+            brains = api.content.find(context=context, depth=1,
+                                           portal_type='Link')
+
+            for brain in brains:
+                obj = brain.getObject()
+                links.append(obj)
+
+        return links
 
     def index(self):
         return super(CitedBy, self).render()
