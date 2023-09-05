3a7b5f7dae41cb0014bc85f1d4140c0cf6f0d1c2
Khairil Yusof <khairil.yusof@gmail.com>
2023-09-05 20:39:43 +0800

    Add Cites to replace and remove CitedBy
    To stay consistent, for relation fields, where many relates to one or a
    few. This makes it more natural as we would only update the referenced
    item ones, and when adding new content, we can then easily find
    and link to the reference item.
    
     * items -> project
     * items -> cites item
     * items -> activity
     * items -> organization

--- src/sinar/resource/behaviors/cites.py
+++ src/sinar/resource/behaviors/cites.py
@@ -9,6 +9,12 @@ from zope.component import adapter
 from zope.interface import Interface
 from zope.interface import implementer
 from zope.interface import provider
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.supermodel.directives import fieldset
+from plone.autoform import directives
+from plone.app.vocabularies.catalog import CatalogSource
 
 
 class ICitesMarker(Interface):
@@ -20,11 +26,41 @@ class ICites(model.Schema):
     """
     """
 
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
+    directives.widget('cites',
+                      RelatedItemsFieldWidget,
+                      pattern_options={
+                          'basePath': '/',
+                          'mode': 'auto',
+                          'favourites': [],
+                      }
+                      )
+
+    cites = RelationList(
+        title='Cites',
+        description=_('''
+                Item that this Resource cites
+                        '''),
+        default=[],
+        value_type=RelationChoice(
+            source=CatalogSource(portal_type=[
+                                 'Project',
+                                 'Activity (Event)',
+                                 'Resource',
+                                 ]
+                                 ),
+        ),
         required=False,
     )
+
+    # fieldset set the tabs on the edit form
+    fieldset(
+        'citation',
+        label=_('Citations'),
+        fields=[
+            'cites',
+        ],
+    )
+
 
 
 @implementer(ICites)
@@ -34,11 +70,11 @@ class Cites(object):
         self.context = context
 
     @property
-    def project(self):
-        if safe_hasattr(self.context, 'project'):
-            return self.context.project
+    def cites(self):
+        if safe_hasattr(self.context, 'cites'):
+            return self.context.cites
         return None
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    @cites.setter
+    def cites(self, value):
+        self.context.cites = value
