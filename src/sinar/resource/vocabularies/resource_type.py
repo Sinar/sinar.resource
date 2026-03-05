6953947846c834977bb8047d8b46808e4bd5bf55
Khairil Yusof <khairil.yusof@gmail.com>
2026-03-05 10:11:40 +0800

    - add Knowledge Respository type - lint - remove u' no longer needed for Python3

--- src/sinar/resource/vocabularies/resource_type.py
+++ src/sinar/resource/vocabularies/resource_type.py
@@ -8,7 +8,6 @@ from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
-
 class VocabItem(object):
     def __init__(self, token, value):
         self.token = token
@@ -18,32 +17,35 @@ class VocabItem(object):
 @implementer(IVocabularyFactory)
 class ResourceType(object):
     """
+    A vocabulary factory for resource types. This is used in the
+    ResourceType field of the Resource content type.
     """
 
     def __call__(self, context):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u'activity', _(u'Activity or Event Report')),
-            VocabItem(u'application', _(u'Application or Product')),
-            VocabItem(u'updates', _(u'Articles, Blogs and Updates')),
-            VocabItem(u'books', _(u'Books')),
-            VocabItem(u'promotional',
-                      _(u'Brochure, Promotional Materials')),
-            VocabItem(u'project_report',
-                      _(u'Project or Organization Reports')),
-            VocabItem(u'data', _(u'Data, Surveys, Fact Sheets')),
-            VocabItem(u'legal', _(u'Legal Documents, Filings, Judgements')),
-            VocabItem(u'legislation', _(u'Legislation, Regulations')),
-            VocabItem(u'newsmedia', _(u'News Media Coverage')),
-            VocabItem(u'periodical', _(u'Newsletter, Journal')),
-            VocabItem(u'policy', _(u'Policy, Strategy or Plan')),
+            VocabItem('activity', _('Activity or Event Report')),
+            VocabItem('application', _('Application or Product')),
+            VocabItem('updates', _('Articles, Blogs and Updates')),
+            VocabItem('books', _('Books')),
+            VocabItem('promotional',
+                      _('Brochure, Promotional Materials')),
+            VocabItem('project_report',
+                      _('Project or Organization Reports')),
+            VocabItem('data', _('Data, Surveys, Fact Sheets')),
+            VocabItem('kmrepo', _('Knowledge Respository')),
+            VocabItem('legal', _('Legal Documents, Filings, Judgements')),
+            VocabItem('legislation', _('Legislation, Regulations')),
+            VocabItem('newsmedia', _('News Media Coverage')),
+            VocabItem('periodical', _('Newsletter, Journal')),
+            VocabItem('policy', _('Policy, Strategy or Plan')),
             VocabItem('presentation', ('Panel, Presentation')),
-            VocabItem(u'pressstatement',
-                    _(u'Press Statement or News Release')),
-            VocabItem(u'research', _(u'Research Reports, Working Paper')),
-            VocabItem(u'training',
-                      _(u'Training Material, Guides, Organizing/Educational Materials')),
+            VocabItem('pressstatement',
+                      ('Press Statement or News Release')),
+            VocabItem('research', _('Research Reports, Working Paper')),
+            VocabItem('training',
+                      _('Training Material, Guides, Organizing/Educational Materials')),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.
